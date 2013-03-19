174cf4a99098b69e1bf5e9f5d3a83980f2a24427
edoshor <edoshor@gmail.com>
2013-03-19 16:57:37 +0200

    Simple tcp socket server to mock real a transcoder

---
+++ lib/MockTxcoder.rb
@@ -0,0 +1,42 @@
+require 'socket'
+require 'thread'
+require 'json'
+
+
+t1 = Thread.new do
+  server = TCPServer.open(10000)
+  loop do
+    client = server.accept
+    puts "#{Time.now}: serving client from #{client.peeraddr}"
+    client.puts(Time.now.ctime)
+    client.puts 'Closing the connection. Bye!'
+    client.close
+  end
+end
+
+t2 = Thread.new do
+  server = TCPServer.open(11000)
+  loop do
+    client = server.accept
+    puts "#{Time.now}: serving load status for client from #{client.peeraddr}"
+
+    result = JSON.generate( { cpuload: '%2.2f %' % (rand * 100),
+        cputemp: [{:'0' => '%2.2f C' % (rand * 100)},
+                  {:'1' => '%2.2f C' % (rand * 100)}]})
+
+    headers = [
+        'HTTP/1.1 200 OK',
+        "Date: #{Time.now}",
+        'Server: Mock Transcoder',
+        'Content-Type: application/json; charset=iso-8859-1',
+        "Content-Length: #{result.length}\r\n\r\n"].join("\r\n")
+    client.puts headers
+    client.puts result
+    client.close
+  end
+end
+
+puts 'Mock transcoder is running.'
+
+t1.join
+t2.join
