854b66cea67b455a166a0d7585dc7b93e3e2a273
Edo Shor <edoshor@gmail.com>
2014-01-19 19:29:33 +0200

    adjust thin config to capistrano deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -45,10 +45,8 @@ namespace :deploy do
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      within release_path do
-        #with rails_env: fetch(:rails_env) do
-          execute :bundle, 'exec thin -C config/thin.yml restart'
-        #end
+      within current_path do
+        execute :bundle, 'exec thin -C config/thin.yml restart'
       end
     end
   end
