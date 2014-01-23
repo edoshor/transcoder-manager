1746ac4bdca5e21571c49cd01908508b56b33baf
Edo Shor <edoshor@gmail.com>
2014-01-23 05:03:59 +0200

    Update mina deploy.rb to start or restart thin according to its state.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,39 +1,23 @@
 require 'mina/bundler'
 require 'mina/rails'
 require 'mina/git'
-require 'mina/rbenv'  # for rbenv support. (http://rbenv.org)
+require 'mina/rbenv'
 
 set :domain, 'staging'
 set :deploy_to, '/var/www/transcoder-manager'
 set :repository, 'https://github.com/edoshor/transcoder-manager'
 set :branch, 'master'
-
-# Manually create these paths in shared/ (eg: shared/config/database.yml) in your server.
-# They will be linked in the 'deploy:link_shared_paths' step.
 set :shared_paths, %w(config/thin.yml config/logging-production.yml)
+set :user, 'deploy'
 
-# Optional settings:
-set :user, 'deploy'    # Username in the server to SSH to.
-#   set :port, '30000'     # SSH port number.
-
-# This task is the environment that is loaded for most commands, such as
-# `mina deploy` or `mina rake`.
 set :rbenv_path, '/usr/local/rbenv'
 task :environment do
   # required for system wide installation of rbenv
   queue %{export RBENV_ROOT=#{rbenv_path}}
 
-  # If you're using rbenv, use this to load the rbenv environment.
-  # Be sure to commit your .rbenv-version to your repository.
   invoke :'rbenv:load'
-
-  # For those using RVM, use this to load an RVM version@gemset.
-  # invoke :'rvm:use[ruby-1.9.3-p125@default]'
 end
 
-# Put any custom mkdir's in here for when `mina setup` is ran.
-# For Rails apps, we'll make some of the shared paths that are shared between
-# all releases.
 task :setup => :environment do
   queue! %[mkdir -p "#{deploy_to}/shared/log"]
   queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/log"]
@@ -42,33 +26,32 @@ task :setup => :environment do
   queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/config"]
 
   queue! %[touch "#{deploy_to}/shared/config/thin.yml"]
-  queue  %[echo "-----> Be sure to edit 'shared/config/thin.yml'."]
+  queue %[echo "-----> Be sure to edit 'shared/config/thin.yml'."]
 
   queue! %[touch "#{deploy_to}/shared/config/logging-production.yml"]
-  queue  %[echo "-----> Be sure to edit 'shared/config/logging-production.yml'."]
+  queue %[echo "-----> Be sure to edit 'shared/config/logging-production.yml'."]
 end
 
 desc 'Deploys the current version to the server.'
 task :deploy => :environment do
   deploy do
-    # Put things that will set up an empty directory into a fully set-up
-    # instance of your project.
     invoke :'git:clone'
     invoke :'deploy:link_shared_paths'
     invoke :'bundle:install'
-    #invoke :'rails:db_migrate'
-    #invoke :'rails:assets_precompile'
 
     to :launch do
-      queue 'bundle exec thin -C config/thin.yml restart'
+      queue %{
+          if [ -f $(cat config/thin.yml | grep pid: | sed '/^pid: */!d; s///;q') ];
+          then
+            echo "thin is up. restarting"
+            bundle exec thin -C config/thin.yml restart
+            exit
+          else
+            echo "thin is down. starting"
+            bundle exec thin -C config/thin.yml start
+            exit
+          fi
+      %}
     end
   end
 end
-
-# For help in making your deploy script, see the Mina documentation:
-#
-#  - http://nadarei.co/mina
-#  - http://nadarei.co/mina/tasks
-#  - http://nadarei.co/mina/settings
-#  - http://nadarei.co/mina/helpers
-
